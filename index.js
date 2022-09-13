1a0f13cec50997fc200ff7d2ee3d33e35b2567da
umudik <seyitumutbicer@gmail.com>
2022-09-13 16:15:47 +0300

    dev

--- index.js
+++ index.js
@@ -2,6 +2,5 @@
     const fookie = require("fookie");
     await fookie.init()
     await fookie.use(require("fookie-server"))
-    await fookie.use(require("fookie-cache").server)
     await fookie.listen(2626)
 })()
